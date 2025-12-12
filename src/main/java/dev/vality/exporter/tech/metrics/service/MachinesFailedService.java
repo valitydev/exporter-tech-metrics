a6a8bc5ffbab6c9f837d2ee2421b01fc8a821cdc
Gretchen-z <rysrit@gmail.com>
2025-12-12 11:04:45 +0300

    Fix tags

--- src/main/java/dev/vality/exporter/tech/metrics/service/MachinesFailedService.java
+++ src/main/java/dev/vality/exporter/tech/metrics/service/MachinesFailedService.java
@@ -118,7 +118,7 @@ public class MachinesFailedService {
                 Tag.of("provider_id", providerId),
                 Tag.of("provider_name", providerName),
                 Tag.of("terminal_id", terminalId),
-                Tag.of("terminal_id", terminalName),
+                Tag.of("terminal_name", terminalName),
                 Tag.of("currency_name", currencyCode)
         );
     }
